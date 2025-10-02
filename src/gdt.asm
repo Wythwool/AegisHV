e8eb5c70422e33e26ab403bba310817fde5b3af0
Mono GlaShyiz <varffdg@gmail.com>
2025-10-03 02:11:49 +0300

    Update gdt.asm

--- src/gdt.asm
+++ src/gdt.asm
@@ -5,9 +5,9 @@ global gdt64_ptr
 SECTION .rodata
 align 8
 gdt64:
-    dq 0x0000000000000000        ; null
-    dq 0x00AF9A000000FFFF        ; code: base=0, limit=FFFFF, G=1, 64-bit
-    dq 0x00AF92000000FFFF        ; data: base=0, limit=FFFFF, G=1
+    dq 0x0000000000000000
+    dq 0x00AF9A000000FFFF
+    dq 0x00AF92000000FFFF
 
 gdt64_ptr:
     dw gdt64_end - gdt64 - 1
