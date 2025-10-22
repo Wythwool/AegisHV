5b240bda65fd8e71472c04de53f7ca01a7211a66
Mono GlaShyiz <varffdg@gmail.com>
2025-10-23 01:51:02 +0300

    Delete hv directory

--- hv/x86/vmcs.c
+++
@@ -1,8 +0,0 @@
-#include "vmcs.h"
-#include "../common/log.h"
-
-int vmx_setup_and_launch(uint64_t guest_entry_gpa, uint64_t guest_stack_gpa, uint64_t eptp){
-    (void)guest_entry_gpa; (void)guest_stack_gpa; (void)eptp;
-    hv_log("INFO", "VMX disabled in this build; run loop ends");
-    return -1;
-}
--- hv/x86/vmx.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <stdint.h>
-int vmx_init(void);
-int vmx_shutdown(void);
-void vmx_run_loop(void);
