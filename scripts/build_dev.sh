7b11d4f54764db484d5da3b2b1cd876d7f37fbcd
Mono GlaShyiz <varffdg@gmail.com>
2025-10-23 01:50:56 +0300

    Delete scripts directory

--- scripts/build_dev.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-pushd devharness >/dev/null
-cargo build --release
-popd >/dev/null
-pushd userspace/aegisd >/dev/null
-cargo build --release
-popd >/dev/null
-echo "OK"
